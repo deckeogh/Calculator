729ca4b224efc289f706ccb3ae1c7e29d2ce1576
Declan Keogh <declankeogh01@gmail.com>
2017-11-02 17:05:39 +0100

    remove calculator elements Insert commentary on approach include timer code

--- Calculator/Controllers/MathController.cs
+++ Calculator/Controllers/MathController.cs
@@ -1,6 +1,7 @@
 ﻿//MathController.cs file.
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,48 +11,30 @@ namespace WepAPI_Demo.Controllers
 {
     public class MathController : ApiController
     {
-        [HttpGet]
-        public string Add(int value1, int value2)
-        {
-            return (value1 + value2).ToString();
-        }
-
-        [HttpGet]
-        public int Substract(int value1, int value2)
-        {
-            return value1 - value2;
-        }
-
-        [HttpGet]
-        public int Multiply(int value1, int value2)
-        {
-            return value1 * value2;
-        }
-
-        [HttpGet]
-        public int Divide(int value1, int value2)
-        {
-            return value1 / value2;
-        }
 
         [HttpGet]
         public string Calculate(string txtString)
         {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            // Splits the numbers into a string array and converts it into an integer array
             string[] caseList = txtString.Split(' ');
             int[] caseIntegers = Array.ConvertAll(caseList, s => int.Parse(s));
             double sumScores = 0;
-            string[] stats = { "", "" };
             System.Numerics.BigInteger permutations = 1;
-            // int permutations = 1;
+            // iterates through the numbers to see how many numbers (elementScore) are greater than or equal to itself
+            // adds (n+1) / (k+1) to sumScores
+            // calculates the permutations
             for (var i = 0; i <= caseIntegers.Length - 1; i++)
             {
                 int elementScore = caseIntegers.Count(item => item >= caseIntegers[i]);
                 sumScores = sumScores + ((double)(caseIntegers.Length + 1) / (double)(elementScore + 1));
                 permutations = permutations * (i + 1);
-                stats = new string[] { sumScores.ToString("0.00"), permutations.ToString("#,###") };
             }
-            //return permutations;
-            return "Average Score: " + sumScores.ToString("0.00") + ",\n Permutations: " + permutations.ToString("#,###");
+            stopWatch.Stop();
+            // Returns detailed string output to client.html as txtResult
+            return "Average Score: " + sumScores.ToString("0.00") + ",\n Permutations: " 
+                + permutations.ToString("#,###") + " Calculation time: " + stopWatch.ElapsedMilliseconds.ToString();
         }
 
         [HttpGet]
