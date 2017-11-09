f77135b9796c3490c1f94b30ac560e0b562877ff
Robert Sirre <robert@cs.nl>
2017-11-09 10:39:38 +0100

    Split logic from presentation, added unit tests and xml docs

--- VerticalSticksCalculatorTests/Properties/AssemblyInfo.cs
+++ VerticalSticksCalculatorTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Calculator")]
-[assembly: AssemblyDescription("A web-based calculator for solving 'Vertical sticks'")]
+[assembly: AssemblyTitle("VerticalSticksCalculatorTests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyProduct("VerticalSticksCalculatorTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09bcd3bb-02f4-4111-8ec2-d4c7b9726141")]
+[assembly: Guid("c3af7781-64c2-45b3-a352-ebbe93f1b13e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
